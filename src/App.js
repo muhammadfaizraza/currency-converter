88268dec7a12ddcdfbdfdbe308fa330cecd6cb82
zaibjeeofficial <muhammadfaizraza48@gmail.com>
2024-11-24 23:08:31 +0500

    env updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Main from "./Pages/Main";
 
 function App() {
-  console.log(process.env.REACT_APP_BASE_URL, "<==");
   return (
     <div className="App">
       <Main />
--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -1,13 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import getCurrencies from "./Reducers/getCurrenciesReducer";
 import convertCurrency from "./Reducers/convertCurrencyReducer";
-import storage from "redux-persist/lib/storage"; // Default: localStorage
+import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 
 const convertCurrencyPersistConfig = {
   key: "convertCurrency",
   storage,
-  whitelist: ["historyData"], // Only persist the historyData field
+  whitelist: ["historyData"],
 };
 const rootReducer = combineReducers({
   getCurrencies: getCurrencies,
